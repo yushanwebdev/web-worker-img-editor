4beb22013566250f463e060fe9bc9d2e1a76ed4e
yushan <yushanwebdev@gmail.com>
2022-04-15 16:48:41 +0530

    feat: Render the image in the Canvas using createImageBitmap()

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ const $input = document.getElementById("input");
 const $preview = document.getElementById("preview");
 const previewCtx = $preview.getContext("2d");
 
-const fileReader = new FileReader();
 const image = new Image();
 
 function applyFilter() {
@@ -18,19 +17,11 @@ function applyFilter() {
   previewCtx.putImageData(imageData, 0, 0);
 }
 
-image.addEventListener("load", (e) => {
-  $preview.width = image.width;
-  $preview.height = image.height;
-  previewCtx.drawImage(image, 0, 0);
-  applyFilter();
-});
-
-fileReader.addEventListener("load", (e) => {
-  const base64 = e.target.result;
-  image.src = base64;
-});
-
 $input.addEventListener("change", (e) => {
   const file = e.target.files[0];
-  fileReader.readAsDataURL(file);
+  createImageBitmap(file).then((bitmap) => {
+    $preview.width = bitmap.width;
+    $preview.height = bitmap.height;
+    previewCtx.drawImage(bitmap, 0, 0);
+  });
 });
